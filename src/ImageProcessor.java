7b9f91a86f50da6ac36b17ad15defd8743f0efd3
PurpleBottle <eric.fehringer@gmail.com>
2017-04-21 17:05:31 -0500

    Finished ImageProcessor and Commented Everything on Image Processor

--- src/ImageProcessor.java
+++ src/ImageProcessor.java
@@ -1,78 +1,88 @@
 import java.awt.Color;
 import java.util.ArrayList;
 
+//Created by Eric Fehringer 
+
 public class ImageProcessor {
 	
-	Picture pic;
-
+	Picture pic; //the picture that will be manipulated
+	
 	public ImageProcessor(String imageFile){
 		pic = new Picture(imageFile);
 	}
+
+	//reduces the widith  by calling the reduceWidthbyOne() the number of
+	//times to get to x*W
 	
 	public Picture reduceWidth(double x){
 		double width = pic.width()*x;
-		
 		int decrement = (int) Math.floor(width);
 		
-		int f = 0;
 		for (int i = decrement ; i>0 ;i--)
 		{
-			System.out.println(f);
 			reduceWidthbyOne();
-			f++;
 		}
 		return pic;
-		
 	}
-	
+	//follows the algorithm given in the homework to find the optimal
+	//pixels to remove when reducing an image
 	private void reduceWidthbyOne()
 	{
 		int height = pic.height();
-		
 		int width = pic.width();
 		
-		
+		//creates I[][] or Importance(M[i,j])
 		int[][] I = new int[height][width];
 		for(int h = 0; h < height;h++)
 		{
 			for(int w = 0; w <width;w++)
 			{
-				if(h!=0 && h!=height-1 && w!=0 && w!= width-1)//when its not an edge case
+				//when its not an edge case
+				if(h!=0 && h!=height-1 && w!=0 && w!= width-1)
 				{
 					I[h][w] = Dist(pic.get(w-1, h), pic.get(w+1, h)) + Dist(pic.get(w, h+1),  pic.get(w, h-1) );
 				}
-				else if(h==0  && w!=0 && w!= width-1) //when height is zero, it wraps around to grab value
+				//when height is zero, it wraps around to grab value
+				else if(h==0  && w!=0 && w!= width-1) 
 				{
 					I[h][w] = Dist(pic.get(w-1, h), pic.get(w+1, h)) + Dist(pic.get(w, h+1),  pic.get(w, height-1) );
 				}
-				else if(h==height-1 && w!=0 && w!= width-1)//when height is at capacity it wraps around
+				//when height is at max it wraps around
+				else if(h==height-1 && w!=0 && w!= width-1)
 				{
 					I[h][w] = Dist(pic.get(w-1, h), pic.get(w+1, h)) + Dist(pic.get(w, 0),  pic.get(w, height-1) );
 				}
-				else if(h!=0 && h!=height-1 && w==0 ) //when the widith is at the beginning it wraps around
+				//when the widith is at the beginning it wraps around
+				else if(h!=0 && h!=height-1 && w==0 ) 
 				{
 					I[h][w] = Dist(pic.get(width-1, h), pic.get(w+1, h)) + Dist(pic.get(w, h+1),  pic.get(w, h-1) );
 				}
-				else if(h!=0 && h!=height-1 &&  w== width-1) //when the width is at max, it wraps around 
+				//when the width is at max, it wraps around 
+				else if(h!=0 && h!=height-1 &&  w== width-1) 
 				{
 					I[h][w] = Dist(pic.get(w-1, h), pic.get(0, h)) + Dist(pic.get(w, h+1),  pic.get(w, h-1) );
 				}
-				else if(h==0 && w == 0) // 0, 0 corner case
+				// 0, 0 corner case
+				else if(h==0 && w == 0) 
 				{
 					I[h][w] = Dist(pic.get(width-1, h), pic.get(w+1, h)) + Dist(pic.get(w, h+1),  pic.get(w, height-1) );
 				}
-				else if(h==height-1 && w == 0) // height-1, 0 corner case
+				// height-1, 0 corner case
+				else if(h==height-1 && w == 0) 
 				{
 					I[h][w] = Dist(pic.get(width-1, h), pic.get(w+1, h)) + Dist(pic.get(w, 0),  pic.get(w, height-1) );
 				}
-				else if(h==0 && w==width-1) //0, width-1 corner case
+				//0, width-1 corner case
+				else if(h==0 && w==width-1) 
 				{
 					I[h][w] = Dist(pic.get(w-1, h), pic.get(0, h)) + Dist(pic.get(w, h+1),  pic.get(w, height-1) );
 				}
-				else if(h==height-1 && w==width-1) //heigh-1, width-1 corner Case
+				//heigh-1, width-1 corner Case
+				else if(h==height-1 && w==width-1) 
 				{
 					I[h][w] = Dist(pic.get(w-1, h), pic.get(0, h)) + Dist(pic.get(w, 0),  pic.get(w, h-1) );
 				}
+				//otherwise it prints the point at which it has an error
 				else{
 					System.out.println("error at ("+h +","+w+")");
 				}
@@ -80,32 +90,31 @@ public class ImageProcessor {
 		}
 		
 		
-		
-
-		
+		//calls minCost to find the optimal pixels to remove
 		ArrayList<Integer> minCostCut = dynamicProgramming.minCostVC(I);
 		
+
 		Picture newPic = new Picture( width-1, height);
 		
-		for(int h = 0; h < height;h++)
+		for(int h = 0; h < height;h++) //populates the new picture
 		{
-			int y = minCostCut.get(h+1);
-			
+			int y = minCostCut.get(2*h+1); //grabs the address of the pixel to remove
+	
 			for(int w = 0; w < y;w++)
 			{
 				newPic.set( w,h, pic.get(w,h));
 			}
-			
+			//skips over pixel that will be removed, and calls w-1 instead of w
 			for(int w = y+1; w < width-1;w++)
 			{
 				newPic.set( w-1,h,pic.get(w,h));
 			}
 		}
 		
-		pic = newPic;
-		
+		pic = newPic;	
 	}
 	
+	//does the RGB comparison of a given pixel
 	private int Dist(Color p, Color q)
 	{
 		int red = p.getRed() - q.getRed();
--- src/gitTest.java
+++ src/gitTest.java
@@ -1,20 +1,31 @@
 import java.util.ArrayList;
-
+import java.awt.Color;
 public class gitTest {
 
 	public static void main(String[] args) {
-		int[][] s = { {7, 5 ,3 ,1 , 9, 4}, {7, 5 ,3 ,1 , 9, 4}, {7, 5 ,3 ,1 , 9, 4}, {7, 5 ,3 ,1 , 9, 4}, {7, 5 ,3 ,1 , 9, 4}, {7, 5 ,3 ,1 , 9, 4} };
-		int[][] s1 = { {7, 5 ,3, 0 },
-					   {2, 1, 4, 12 },
-					   {5, 0, 10, 4},
-					   {2, 9, 10, 4}
-		};
-		ArrayList<Integer> ret = dynamicProgramming.minCostVC(s1);
-        for(int i = 0; i < ret.size();i=i+2)
-        {
-        	System.out.println(ret.get(i) + ", "+ ret.get(i+1));
-        	//ret.get(i);
-        }
+//		int[][] s = { {7, 5 ,3 ,1 , 9, 4}, {7, 5 ,3 ,1 , 9, 4}, {7, 5 ,3 ,1 , 9, 4}, {7, 5 ,3 ,1 , 9, 4}, {7, 5 ,3 ,1 , 9, 4}, {7, 5 ,3 ,1 , 9, 4} };
+//		int[][] s1 = { {7, 5 ,3, 0 },
+//					   {2, 1, 4, 12 },
+//					   {5, 0, 10, 4},
+//					   {2, 9, 10, 4}
+//		};
+//		ArrayList<Integer> ret = dynamicProgramming.minCostVC(s1);
+//        for(int i = 0; i < ret.size();i=i+2)
+//        {
+//        	System.out.println(ret.get(i) + ", "+ ret.get(i+1));
+//        	//ret.get(i);
+//        }
+        
+		//System.out.print((int )5.7);
+		
+		
+       ImageProcessor s = new ImageProcessor("Original.jpg");
+       Picture p = s.reduceWidth(0.5);
+        p.save("test1.jpg");
+
+        
+
+       
 
 	}
 
